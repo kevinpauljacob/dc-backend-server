5221e0390a8c4c9076cb60867dedf2e391101cd8
itaintkevin <kpjtofficial@gmail.com>
2023-10-17 23:50:30 +0530

    fix: Updated config

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,5 @@
 export default {
     port: 3333,
     dbUri: "mongodb+srv://user:test123@cluster0.uyl3b0q.mongodb.net/nfts?retryWrites=true&w=majority",
-    origin: 'http://localhost:3000'
+    origin: 'https://dc-backend-client.vercel.app'
 }
